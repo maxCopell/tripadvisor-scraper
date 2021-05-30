d7f7406f56a57eb8aba6c18bbea30bda456a0036
Gustavo Rudiger <gustavotrudiger@gmail.com>
2021-05-30 15:52:47 +0300

    Feature/limit items (#1)
    * feat: 🎸 limit items to be saved for hotels
    
    * feat: 🎸 limit reviews for hotels
    
    * feat: 🎸 limit items for restaurants
    
    * fix: 🐛 number of reviews limited by maxReviews
    
    * fix: 🐛 do not exit process to stop crawler

---
+++ src/tools/data-limits.js
@@ -0,0 +1,61 @@
+/**
+ * @typedef Config
+ * @type {object}
+ * @property {number} maxItems
+ * @property {number} maxReviews
+ */
+
+const Apify = require('apify');
+const { log } = Apify.utils;
+
+
+/**
+ * @type {Config}
+ */
+let config = {
+    maxItems: 0,
+    maxReviews: 0,
+};
+
+let savedItems = 0;
+
+
+const checkMaxItemsLimit = () =>{
+    if(config.maxItems !== 0 && savedItems >= config.maxItems){
+        log.warning('Reached maxItems limit!');
+    };
+}
+
+const incrementSavedItems = () =>{
+    savedItems += 1;
+    log.debug('Saving:', {savedItems, maxItems:config.maxItems});
+}
+
+
+/**
+ * 
+ * @param {Config} newConfig 
+ */
+const setConfig = (newConfig) => {
+    Object.keys(config).forEach((key )=> {
+        if(newConfig[key]){
+            config[key] = newConfig[key];
+        }
+    });
+}
+
+/**
+ * 
+ * @returns {Config}
+ */
+const getConfig = () => {
+    return config;
+}
+
+
+module.exports = {
+    getConfig,
+    setConfig,
+    incrementSavedItems,
+    checkMaxItemsLimit
+};
